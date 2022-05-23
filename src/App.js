b44538f5569b57ad6be337acfea9cf3536adc30f
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-05-24 00:36:02 +0500

    Logo section completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import Logo from "./components/logos/Logo";
+import Popular from "./components/popular/Popular";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Logo />
+      <Popular />
     </div>
   );
 }
